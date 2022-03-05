8fa3a25081d3c7cf8b9886a17bee31b907cd40ce
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-03-05 17:26:01 +0000

    Create BoomBoom.lua

---
+++ BoomBoom.lua
@@ -0,0 +1,5 @@
+getgenv().Key = "_GRUBHUB_KEY_HERE_"
+getgenv().DiscordWebhook = "Link";
+getgenv().WebhookEnabled = false;
+getgenv().GubVersion = "5.0";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/botdevXD/botdevXD/master/cockhubv5.lua", true))();
